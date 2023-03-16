26cc7480f9d39460e7f3a3c9d9c5822e35e1fccd
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-16 14:45:32 -0700

    4-puts.c files after corrections

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
+
 /**
- * main - a C program that uses puts fuction to  print
+ * main - A program that uses puts fuction to  print
  *
  * Return: 0 (Success)
  */
+
 int main(void)
 {
-	char programming_message;
-
-	programming_message = "\"Programming is like building a multilingual puzzle";
-	puts(programming_message);
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
