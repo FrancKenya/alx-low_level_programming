4396af6786b9cef6efbcb9748647b551903839d1
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-16 14:32:27 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - a C program that uses puts fuction to  print
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char programming_message;
+
+	programming_message = "\"Programming is like building a multilingual puzzle";
+	puts(programming_message);
+	return (0);
+}
