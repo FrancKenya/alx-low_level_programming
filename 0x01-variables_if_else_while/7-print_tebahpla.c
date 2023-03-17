5b8ffdfa74544c1452416dd050b954632533c513
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-17 15:58:16 -0700

    7-print_tebahpla.c and 7-print_tebahpla added

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - prints the lowercase alphabet in reverse
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'z'; x >= 'a'; x--)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
