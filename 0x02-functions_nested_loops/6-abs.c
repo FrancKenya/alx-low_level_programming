b917f60401a803a70dcbac388c4e1290b0cf72e2
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-21 16:53:26 -0700

    6-abs.c added

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+$include "main.h"
+
+/**
+ * _abs - computes the absolute value of an integer
+ * @x: the integer
+ * Return: the abs of the integer
+ */
+int _abs(int x)
+{
+	if (n >= 0)
+	{
+		return (x);
+	}
+	{
+	else
+		return (-x);
+	}
+}
