92e3e17c6fc8bd10e22ee13ad1154ab0a25ed34d
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-21 09:38:31 -0700

    1-alphabet.c and 1-alphabet added after first corrections

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,17 +1,18 @@
 #include "main.h"
 
+void print_alphabet(void);
+
 /**
  * main -  prints the alphabet, in lowercase
  * Return: 0
  */
-
-void print_alphabet(void);
 
 int main(void)
 {
 	print_alphabet();
 	return (0);
 }
+
 void print_alphabet(void)
 {
 	char x;
