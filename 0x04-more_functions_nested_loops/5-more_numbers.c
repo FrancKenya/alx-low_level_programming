73de05b3af91276597e4ccabfa82c5f748822203
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-23 16:02:45 -0700

    5-more_numbers.c third correction added

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,13 +8,13 @@ void more_numbers(void)
 {
 	int a, b;
 
-	for (a = 0; a < 10; ++a)
+	for (a = 1; a <= 10; a++)
 	{
-		for (b = 0; b <= 14; ++b)
+		for (b = 0; b <= 14; b++)
 		{
-			if (b > 9)
+			if (b >= 10)
 			{
-				_putchar((b / 10) + '0');
+				_putchar('1');
 			}
 		else
 		{
