e3f8c10fef3593cd3d9d2aad1c5dcc69298580e3
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-23 10:41:41 -0700

    5-more_numbers.c final corrections

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -14,13 +14,13 @@ void more_numbers(void)
 		{
 			if (b > 9)
 			{
-				_putchar((b / 10) + 48);
+				_putchar((b / 10) + '0');
 			}
 		else
 		{
-			_putchar((b % 10) + 48);
+			_putchar((b % 10) + '0');
 		}
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
