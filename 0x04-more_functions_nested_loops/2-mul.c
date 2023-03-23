28ef4f8d635078637de9994ea551eeb2ffad5e77
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-23 07:35:01 -0700

    2-mul.c added

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * mul -  multiplies two integers
+ * @a: Integer being multiplied
+ * @b: Integer being multiplied
+ * Return: results
+ */
+
+int mul(int a, int b)
+{
+	int results;
+
+	results = a * b;
+	return (results);
+}
