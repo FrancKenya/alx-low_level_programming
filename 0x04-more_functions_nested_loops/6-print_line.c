614b6152ae5d8890352dfc935a6afd659a73f666
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-23 17:12:37 -0700

    6-print_line.c readded

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,18 +8,18 @@
 
 void print_line(int n)
 {
+	int x;
+
 	if (n <= 0)
 	{
 		_putchar('\n');
 	}
 	else
 	{
-		int x;
-
 		for (x = 0; x < n; x++)
 		{
 			_putchar(95);
 		}
+		_putchar('\n');
 	}
-	_putchar('\n');
 }
