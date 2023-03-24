3a333d1332f670df2aa0a4169acd4b17c2d3e7bd
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-23 17:19:38 -0700

    9-fizz_buzz added

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,4 +1,5 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
  * main - The “Fizz-Buzz test”
@@ -12,7 +13,7 @@ int main(void)
 
 	for (x = 1; x <= 100; x++)
 	{
-		if (x % 3 == 0 && x % 5 == 0)
+		if ((x % 3 == 0) && (x % 5 == 0))
 		{
 			printf("FizzBuzz ");
 		}
