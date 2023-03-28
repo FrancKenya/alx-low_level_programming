97b590a9fcab7e8bf287540b8be7282763db8156
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-03-27 19:54:59 -0700

    Task 100

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,47 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * @s: string to be converted 
+ * Return: the integer converted
+ */
+
+int _atoi(char *s)
+{
+	int a, b, c, d, e, f;
+
+	a = 0;
+       	b = 0;
+       	n = 0;
+       	d = 0;
+       	e = 0;
+       	f = 0;
+	while (s[d] != '\0')
+	{
+		d++;
+	}
+	while (a < d && e == 0)
+	{
+		if (s[a] == '-')
+		{
+			++d;
+		}
+		if (s[a] >= '0' && s[a] <= '9')
+		{
+			f = s[a] - '0';
+		      if (b % 2)
+		      {
+			      f = -f;
+		      }
+		      n = n * 10 + f;
+		      e = 1;
+		      if (s[a + 1] < '0' || s[a + 1] > '9')
+			      break;
+		      e = 0;
+		}
+		a++;
+	}
+	if (e == 0)
+		return (0);
+	return (n);
+}
