c888d5d12d1a9511e23be0b7c102ff9238ce0f7c
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-10 05:11:52 -0700

    Task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - A program that prints its name followed by a new line
+ * @argc: arguments count
+ * @argv: array of pointers to string
+ *
+ * Return: 0 success
+ */
+
+int main(int argc __attribute__((unused)), char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
