435eb633fd25adbec1d68d2e5d357a8c4272f840
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-11 19:48:33 -0700

    Task 3

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -42,13 +42,12 @@ int **alloc_grid(int width, int height)
 			return (NULL);
 		}
 	}
-	while (i < height)
+	for (i = 0; i < height; i++)
 	{
 		for (j = 0; j < width; j++)
 		{
 			x[i][j] = 0;
 		}
-		i++;
 	}
 	return (x);
 }
