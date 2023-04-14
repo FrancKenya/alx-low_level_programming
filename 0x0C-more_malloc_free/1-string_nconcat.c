0282e62a04807964189e8cc069104f0c0948108a
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-13 18:46:42 -0700

    Task 1 - 1. string_nconcat

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -19,11 +19,13 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 	if (s1 != NULL)
 	{
-		for (len1 = 0; s1[len1] != '\0'; len1++); /* length of s1 */
+		for (len1 = 0; s1[len1] != '\0'; len1++)
+			;
 	}
 	if (s2 != NULL)
 	{
-		for (len2 = 0; s2[len2] != '\0' && len2 <  n; len2++); /* length of s2 */
+		for (len2 = 0; s2[len2] != '\0' && len2 <  n; len2++)
+			;
 	}
 	if (n < len2)
 	{
