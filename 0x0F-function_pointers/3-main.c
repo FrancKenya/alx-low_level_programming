5aa5006744258b217737f85c02cef4eb083ef033
Francis G Waihiga <francisgwaihiga@gmail.com>
2023-04-21 18:48:42 -0700

    Task 3 - 3-main.c

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -29,12 +29,7 @@ int main(int argc, char **argv)
 	num1 = atoi(argv[1]);
 	num2 = atoi(argv[3]);
 
-	if (*op == '/' && num2 == 0)
-	{
-		printf("Error\n");
-		exit(100);
-	}
-	if (*op == '%' && num2 == 0)
+	if ((*op == '/' && num2 == 0) || (*op == '%' && num2 == 0))
 	{
 		printf("Error\n");
 		exit(100);
