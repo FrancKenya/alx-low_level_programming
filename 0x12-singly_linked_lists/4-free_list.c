99ccd61e0ae625a6a8cbbe17880949fcb6d86a73
FrancKenya <francisgwaihiga@gmail.com>
2023-04-28 05:04:22 +0300

    Task 4

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * free_list - a function that frees a list_t list
+ * @head: points to the list being freed
+ *
+ * Return: void
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current;
+
+	while (head != NULL)
+	{
+		current = head;
+		head = head->next;
+		free(current->str);
+		free(current);
+	}
+}
