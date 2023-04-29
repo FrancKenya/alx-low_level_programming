d7a73dda5e37296e7c427d88d020be608a38cd8f
francisgwaihiga <francisgwaihiga@gmail.com>
2023-04-29 21:30:48 +0000

    Task 5

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/**
+* print_string - prints two strings before main
+* Return: void
+*/
+void print_string(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
