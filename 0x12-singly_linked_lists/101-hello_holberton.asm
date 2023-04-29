c330975e44884a9f5581d0b04791f53b773709df
francisgwaihiga <francisgwaihiga@gmail.com>
2023-04-29 22:09:28 +0000

    Task 6

---
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -0,0 +1,20 @@
+section .data
+format db "Hello, Holberton\n", 0
+
+section .text
+global main
+
+extern printf
+
+main:
+push rbp
+mov rbp, rsp
+
+mov rdi, format
+xor eax, eax
+call printf
+
+mov rsp, rbp
+pop rbp
+xor eax, eax
+ret
