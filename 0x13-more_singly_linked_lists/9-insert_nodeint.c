a55437ab957adec9968ea68e404492e6805ebc61
francisgwaihiga <francisgwaihiga@gmail.com>
2023-05-01 20:17:15 +0000

    betty error corrected

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -9,43 +9,43 @@
 */
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
-	listint_t *current_node, *new_node;
-	unsigned int i = 0;
+listint_t *current_node, *new_node;
+unsigned int i = 0;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-	current_node = *head;
-	while (i < idx && current_node != NULL)
-	{
-		current_node = current_node->next;
-		i++;
-	}
-	if (i < idx)
-	{
-		return (NULL);
-	}
-	new_node = malloc(sizeof(listint_t));
-	if (new_node == NULL)
-	{
-		return (NULL);
-	}
-	new_node->n = n;
-	if (idx == 0)
-	{
-		new_node->next = *head;
-		*head = new_node;
-	}
-	else
-	{
-		new_node->next = current_node;
-		current_node = *head;
-		for (i = 0; i < idx - 1 && current_node != NULL; i++)
-		 {
-			 current_node = current_node->next;
-		 }
-		 current_node->next = new_node;
-	}
-	return (new_node);
+if (head == NULL)
+{
+return (NULL);
+}
+current_node = *head;
+while (i < idx && current_node != NULL)
+{
+current_node = current_node->next;
+i++;
+}
+if (i < idx)
+{
+return (NULL);
+}
+new_node = malloc(sizeof(listint_t));
+if (new_node == NULL)
+{
+return (NULL);
+}
+new_node->n = n;
+if (idx == 0)
+{
+new_node->next = *head;
+*head = new_node;
+}
+else
+{
+new_node->next = current_node;
+current_node = *head;
+for (i = 0; i < idx - 1 && current_node != NULL; i++)
+{
+current_node = current_node->next;
+}
+current_node->next = new_node;
+}
+return (new_node);
 }
