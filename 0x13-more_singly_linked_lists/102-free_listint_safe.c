da6ed595aac8c7ac527bd17a05b983ef17d1ca2f
francisgwaihiga <francisgwaihiga@gmail.com>
2023-05-02 18:03:42 +0000

    betty error corrected

--- 0x13-more_singly_linked_lists/102-free_listint_safe.c
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -12,7 +12,7 @@ size_t free_listint_safe(listint_t **h)
 	size_t i = 0;
 	listint_t *current, *next;
 
-	if (!h)
+	if (!h || !*h)
 	{
 		return (i);
 	}
@@ -20,7 +20,7 @@ size_t free_listint_safe(listint_t **h)
 	while (current)
 	{
 		next = current->next;
-		free (current);
+		free(current);
 		i++;
 		if ((void *)next <= (void *)current)
 		{
