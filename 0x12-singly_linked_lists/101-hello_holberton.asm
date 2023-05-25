7c4f5e8e693709cc72a77e312d75491d88b1104f
francisgwaihiga <francisgwaihiga@gmail.com>
2023-05-25 21:11:22 +0000

    101-hello_holberton.asm

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,18 +1,13 @@
-section .dt
-	hello db "Hello, Holberton", 0
-	format db "%s", 10, 0 ; %s: string, 10: newline, 0: null terminator
+global    main
+extern    printf
+
+section .data
+    format db `Hello, Holberton\n`,0
+
 section .txt
-	extern printf
-
-global main
 main:
-	sub rsp, 8
-	mov rdi, format
-	mov rsi, hello
-	xor eax, eax
-	call printf
-	add rsp, 8
-
-	mov eax, 60  ; exit system call number
-	xor edi, edi ; exit status 0
-	syscall
+	mov edi, format
+	xor   eax, eax
+	call  printf
+	mov   eax, 0
+	ret
