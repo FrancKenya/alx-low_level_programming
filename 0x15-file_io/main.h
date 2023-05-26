c7728dfa29528907ea7ca0e4a107421473c6f315
francisgwaihiga <francisgwaihiga@gmail.com>
2023-05-26 15:29:07 +0000

    main.h

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -6,18 +6,40 @@
 #include <elf.h>
 #include <stdio.h>
 
-ssize_t read_textfile(const char *filename, size_t letters);
-int create_file(const char *filename, char *text_content);
-int append_text_to_file(const char *filename, char *text_content);
-void elf_checking(unsigned char *e_ident);
-void print_a(unsigned char *e_ident);
-void print_o(unsigned char *e_ident);
-void print_t(unsigned int e_type, unsigned char *e_ident);
-void print_mag(unsigned char *e_ident);
-void print_c(unsigned char *e_ident);
-void print_d(unsigned char *e_ident);
-void print_v(unsigned char *e_ident);
-void print_a(unsigned char *e_ident);
-void print_e(unsigned long int e_entry, unsigned char *e_ident);
-void closing_elf(int elf);
+/**
+* struct ElfHeader - stands for ELF
+* @e_ident: ELF identity
+* @e_type: file type
+* @e_machine: Target
+* @e_version: Version of file
+* @e_entry: Entry point
+* @e_phoff: Program header
+* @e_shoff: Section header
+* @e_flags: Processor
+* @e_ehsize: ELF header
+* @e_phentsize: Size
+* @e_phnum: Number
+* @e_shentsize: Size of section entry
+* @e_shnum: Number of section entries header
+* @e_shstrndx: index of section.
+*/
+
+typedef struct ElfHeader
+{
+	unsigned char e_ident[EI_NIDENT];
+	uint16_t e_type;
+	uint16_t e_machine;
+	uint32_t e_version;
+	uint64_t e_entry;
+	uint64_t e_phoff;
+	uint64_t e_shoff;
+	uint32_t e_flags;
+	uint16_t e_ehsize;
+	uint16_t e_phentsize;
+	uint16_t e_phnum;
+	uint16_t e_shentsize;
+	uint16_t e_shnum;
+	uint16_t e_shstrndx;
+} ElfHeader;
+
 #endif
