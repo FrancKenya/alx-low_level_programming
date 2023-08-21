9564885466b01d9cc12a3e2c052f5b3ae02b5f6c
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-21 14:09:32 +0000

    output error handled

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/bash
 gcc -Wall -Wextra -Werror -pedantic -c -fPIC *.c
-# use gcc and -shared (indicate it's a dynamic library) to link obkect files
 gcc -shared -o liball.so *.o
-# Append directory
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
