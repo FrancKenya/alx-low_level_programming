1617f25df1ed9ed9b78fa97ab512a7448bcd74b1
francisgwaihiga <francisgwaihiga@gmail.com>
2023-08-21 16:49:57 +0000

    requirement error handled

--- 0x18-dynamic_libraries/1-create_dynamic_lib.sh
+++ 0x18-dynamic_libraries/1-create_dynamic_lib.sh
@@ -1,4 +1,10 @@
-#!/usr/bin/bash
-gcc -Wall -Wextra -Werror -pedantic -C -fPIC *.c
+#!/bin/bash
+
+#use gcc -c (not to link) -fPIC(positional independent code for share libraries) 
+gcc -c -fPIC *.c
+
+# include shared flag for shared library
 gcc -shared -o liball.so *.o
+
+#append directory to path
 export LD_LIBRARY_PATH=.:$LD_LIBRARY_PATH
