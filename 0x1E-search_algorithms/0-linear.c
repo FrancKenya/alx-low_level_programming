fb3ac96005e9f3f1d43192036d40f7c70699acce
francis waihiga <francisgwaihiga@gmail.com>
2023-12-17 03:33:00 +0000

    Task 0

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,30 @@
+# include "search_algos.h"
+# include <stdio.h>
+/**
+ * linear_search - A function that uses linear search algorithm to search
+ * an array
+ * @array: Pointer to the first element
+ * @size: Number of elements in that array
+ * @value: Value being searched
+ *
+ * Return: First index where value is located
+ */
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL)
+	{
+		return (-1);
+	}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%lu] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
