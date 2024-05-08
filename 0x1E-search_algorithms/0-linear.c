8bc35f14f9b9007817bc003d3d1cc8f422392d7b
francis waihiga <francisgwaihiga@gmail.com>
2024-05-08 06:24:28 -0700

    Task 1

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,7 +1,8 @@
 # include "search_algos.h"
 # include <stdio.h>
+
 /**
- * linear_search - A function that uses linear search algorithm to search
+ * linear_search - A C function that uses linear search algorithm to search
  * an array
  * @array: Pointer to the first element
  * @size: Number of elements in that array
